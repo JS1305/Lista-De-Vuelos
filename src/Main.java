0d5d4b4689d7c6183d49567bfe275c321ea04850
Sebastian Riveros <sebasriveros1305@gmail.com>
2025-11-17 21:25:34 +0100

    avance en puntos 1,2,4,6

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
+import entities.Vuelo;
+
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 
 public class Main {
     public static void main(String[] args) {
+        LocalDate fechaInicio = LocalDate.of(2024,1,1);
+        LocalDate fechafin = LocalDate.of(2026,12,31);
 
-        }
+        ArrayList<Vuelo> vuelos = new ArrayList<>();
+
+        vuelos.add(new Vuelo(1, "H001-V", "Iberia", "Madrid", "Buenos Aires", LocalDate.of(2025, 5, 20), LocalDate.of(2025, 5, 20)));
+        vuelos.add(new Vuelo(2, "H002-V", "Singapore Airlines", "Bangkok", "Nueva Delhi", LocalDate.of(2025, 7, 17), LocalDate.of(2025, 7, 17)));
+        vuelos.add(new Vuelo(3, "H003-V", "Emirates", "Londres", "Frankfurt", LocalDate.of(2025, 3, 14), LocalDate.of(2025, 3, 14)));
+        vuelos.add(new Vuelo(4, "H004-V", "Qantas", "Singapur", "Roma", LocalDate.of(2025, 4, 25), LocalDate.of(2025, 4, 25)));
+        vuelos.add(new Vuelo(5, "H005-V", "Turkish Airlines", "Atenas", "Estocolmo", LocalDate.of(2025, 8, 3), LocalDate.of(2025, 8, 3)));
+        vuelos.add(new Vuelo(6, "H006-V", "Air Canada", "Nueva York", "Barcelona", LocalDate.of(2025, 12, 31), LocalDate.of(2026, 1, 1)));
+        vuelos.add(new Vuelo(7, "H007-V", "KLM Royal Dutch Airlines", "Ámsterdam", "São Paulo", LocalDate.of(2025, 10, 22), LocalDate.of(2025, 10, 22)));
+        vuelos.add(new Vuelo(8, "H008-V", "Japan Airlines", "Tokio", "Los Ángeles", LocalDate.of(2025, 7, 21), LocalDate.of(2025, 7, 22)));
+        vuelos.add(new Vuelo(9, "H009-V", "LATAM Airlines", "Bogota", "Santiago de chile", LocalDate.of(2025, 3, 18), LocalDate.of(2025, 3, 18)));
+        vuelos.add(new Vuelo(10, "H0010-V", "Qatar Airways", "París", "Ciudad de México", LocalDate.of(2025, 3, 3), LocalDate.of(2025, 3, 3)));
     }
+}
---
+++ src/entities/Vuelo.java
@@ -0,0 +1,60 @@
+package entities;
+
+import java.time.LocalDate;
+
+public class Vuelo {
+    private int id;
+    private String nombreVuelo;
+    private String empresa;
+    private String lugarSalida;
+    private String lugarLlegada;
+    private LocalDate fechaSalida;
+    private LocalDate fechaLlegada;
+
+    public Vuelo(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getNombreVuelo() {
+        return nombreVuelo;
+    }
+    public String getEmpresa() {
+        return empresa;
+    }
+    public String getLugarSalida() {
+        return lugarSalida;
+    }
+    public String getLugarLlegada() {
+        return lugarLlegada;
+    }
+    public LocalDate getFechaSalida() {
+        return fechaSalida;
+    }
+    public LocalDate getFechaLlegada() {
+        return fechaLlegada;
+    }
+    public Vuelo(int id, String nombreVuelo, String empresa, String lugarSalida, String lugarLlegada, LocalDate fechaSalida, LocalDate fechaLlegada) {
+        this.id = id;
+        this.nombreVuelo = nombreVuelo;
+        this.empresa = empresa;
+        this.lugarSalida = lugarSalida;
+        this.lugarLlegada = lugarLlegada;
+        this.fechaSalida = fechaSalida;
+        this.fechaLlegada = fechaLlegada;
+    }
+    @Override
+    public String toString() {
+        return "Vuelo{" +
+                "id=" + id +
+                ", nombreVuelo='" + nombreVuelo + '\'' +
+                ", empresa='" + empresa + '\'' +
+                ", lugarSalida='" + lugarSalida + '\'' +
+                ", lugarLlegada='" + lugarLlegada + '\'' +
+                ", fechaSalida=" + fechaSalida +
+                ", fechaLlegada=" + fechaLlegada +
+                '}';
+    }
+}
